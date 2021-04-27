93d43ef664865928d48e8042cd752286c3c9c14c
gaby205 <c14180055@john.petra.ac.id>
2021-04-27 20:29:52 +0700

    v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,58 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  dataPOST = [];
+  loading: any;
+  post : any ={};
+ 
+  
+  constructor(
+    private http : HttpClient,
+    private loadCtrl : LoadingController,
+    private toastCtrl : ToastController
+  ) {}
 
-  constructor() {}
+  async getDataPost(){
+    
+    this.loading = await this.loadPresent();
+
+    this.http.get("http://jsonplaceholder.typicode.com/posts").subscribe((res : any) =>{
+      this.dataPOST = res;
+      console.log(res);
+      if (this.loading) {
+        this.loading.dismiss();
+      }
+    });
+    
+  }
+
+  ionViewDidEnter() {
+    this.getDataPost();
+  }
+
+  public async loadPresent(): Promise<any>{
+
+    const loading = await this.loadCtrl.create({
+      message: "LOADING ...",
+      backdropDismiss: true
+    })
+    await loading.present();
+
+    return loading;
+
+  }
+
+
+  submit(){
+    this.http.post("http://jsonplaceholder.typicode.com/posts", this.post).subscribe((res : any) => {
+      console.log(res);
+      this.toastCtrl.create({
+        duration : 3000,
+        message : "ID for new Item is "+ res.id
+      }).then(l => l.present())
+    })
+  }
+
+
 
 }
